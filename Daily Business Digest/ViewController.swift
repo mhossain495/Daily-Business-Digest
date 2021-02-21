30aef5bedae3f386adae68ede6e85207efb7c1ac
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2021-02-20 19:13:35 -0500

    Initial Commit

---
+++ Daily Business Digest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily Business Digest
+//
+//  Created by shanaaz begum on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
