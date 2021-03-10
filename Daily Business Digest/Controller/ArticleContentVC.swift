d8696013224ea72d423ab1951f813254c2de357d
Mohammed Hossain <mohammed.hossain4@baruchmail.cuny.edu>
2021-03-10 16:49:51 -0500

    Add date label subview and constraints to display article date and time below author

--- Daily Business Digest/Controller/ArticleContentVC.swift
+++ Daily Business Digest/Controller/ArticleContentVC.swift
@@ -11,12 +11,13 @@ import UIKit
 class ArticleContentVC: UIViewController {
     
     let scrollView = UIScrollView()
-    let articleContentLabel = UILabel()
+    let contentView = UIView()
     let articleTitleLabel = UILabel()
     let articleAuthorLabel = UILabel()
-    let contentView = UIView()
+    let articleDateLabel = UILabel()
+    let articleContentLabel = UILabel()
     
-   
+    
     override func viewDidLoad() {
         view.backgroundColor = .white
     }
@@ -27,18 +28,20 @@ class ArticleContentVC: UIViewController {
         // Add Subviews
         view.addSubview(scrollView)
         scrollView.addSubview(contentView)
-        contentView.addSubview(articleContentLabel)
         contentView.addSubview(articleTitleLabel)
         contentView.addSubview(articleAuthorLabel)
+        contentView.addSubview(articleDateLabel)
+        contentView.addSubview(articleContentLabel)
         
         // Configure layout constraints for Subviews and configure label properties
         setScrollViewConstraints()
         setContentViewConstraints()
-        setContentLabelConstraints()
+        
         setArticleTitleConstraints()
+        setArticleAuthorLabelConstraints()
+        setArticleDateLabelConstraints()
+        setContentLabelConstraints()
         configureLabels()
-        setArticleAuthorLabelConstraints()
-        setArticleAuthorLabelConstraints()
     }
 
     
@@ -49,28 +52,24 @@ class ArticleContentVC: UIViewController {
     
     func configureLabels() {
         articleTitleLabel.numberOfLines = 0
-        articleContentLabel.numberOfLines = 0
         articleAuthorLabel.numberOfLines = 0
+        articleDateLabel.numberOfLines = 0
+        articleContentLabel.numberOfLines = 0
         
         articleTitleLabel.sizeToFit()
-        articleContentLabel.sizeToFit()
         articleAuthorLabel.sizeToFit()
-    
-        articleTitleLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
-        articleAuthorLabel.font = UIFont.italicSystemFont(ofSize: 16.0)
+        articleDateLabel.sizeToFit()
+        articleContentLabel.sizeToFit()
+        
+        articleTitleLabel.font = UIFont.boldSystemFont(ofSize: 20)
+        articleAuthorLabel.font = UIFont.italicSystemFont(ofSize: 16)
+        articleDateLabel.font = UIFont.systemFont(ofSize: 12)
+        articleDateLabel.textColor = UIColor.systemGray
     }
     
 
     
     // Set constraints for labels and views
-    func setContentLabelConstraints() {
-        articleContentLabel.translatesAutoresizingMaskIntoConstraints = false
-        articleContentLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        articleContentLabel.topAnchor.constraint(equalTo: articleAuthorLabel.bottomAnchor, constant: 20).isActive = true
-        articleContentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
-        articleContentLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
-    }
-    
     func setArticleTitleConstraints() {
         articleTitleLabel.translatesAutoresizingMaskIntoConstraints = false
         articleTitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
@@ -83,6 +82,22 @@ class ArticleContentVC: UIViewController {
         articleAuthorLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         articleAuthorLabel.topAnchor.constraint(equalTo: articleTitleLabel.bottomAnchor, constant: 20).isActive = true
         articleAuthorLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+    }
+    
+    func setArticleDateLabelConstraints() {
+        articleDateLabel.translatesAutoresizingMaskIntoConstraints = false
+        articleDateLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        articleDateLabel.topAnchor.constraint(equalTo: articleAuthorLabel.bottomAnchor, constant: 5).isActive = true
+        articleDateLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+    }
+    
+    
+    func setContentLabelConstraints() {
+        articleContentLabel.translatesAutoresizingMaskIntoConstraints = false
+        articleContentLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        articleContentLabel.topAnchor.constraint(equalTo: articleDateLabel.bottomAnchor, constant: 20).isActive = true
+        articleContentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        articleContentLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
     }
     
     func setScrollViewConstraints() {
